92026fee52eda6e9af380f791dcb0e632832f737
TJ <87623562+tzytkowski@users.noreply.github.com>
2023-07-06 23:57:31 -0400

    Added stars and depth of field

--- animate.js
+++ animate.js
@@ -1,9 +1,11 @@
 import * as THREE from "https://unpkg.com/three/build/three.module.js";
 import { OrbitControls } from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js';
 
+
+
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(
-  75,
+  240,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
@@ -13,10 +15,14 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 //CAMERA
-camera.position.set(0, 0, 30);
+camera.position.set(-14, 10, 15);
 const controls = new OrbitControls(camera, renderer.domElement);
-controls.zoomSpeed = 0.5;
+controls.zoomSpeed = 5.5;
 controls.enablePan = true;
+
+//CAMERA HELPER
+const helper = new THREE.CameraHelper( camera );
+scene.add( helper );
 
 //STAGE
 const stage = new THREE.TorusKnotGeometry(5.522, 64, 20, 0, 6.28, 0, 3.42);
@@ -28,74 +34,84 @@ scene.background = spaceTexture;
 
 //LIGHT
 var angle = 0;
-var light = new THREE.DirectionalLight(0xffffff, 2.2, 80, 1);
+var light = new THREE.DirectionalLight(0xFFFFFF, 2.2, 80, 1);
 light.position.set(9, -5, 2);
 scene.add(light);
 
-/*
-var secondlight = new THREE.SpotLight(0xffffff, 3.2, 20, 1);
-secondlight.position.set(3, 0, 29);
+//LIGHT HELPER
+const lightHelper = new THREE.DirectionalLightHelper( light, 5 );
+scene.add( lightHelper );
+
+var secondlight = new THREE.DirectionalLight(0xffffff, 3.2, 20, 1);
+secondlight.position.set(-112, 0, -29);
 scene.add(secondlight);
-*/
+
 
 //TEXTURE
 const planetTexture = new THREE.TextureLoader().load('roughtexture.jpg');
+//SECOND TEXTURE WHICH IS TECHNICALLY A BUMP 
+const planetTextureTwo = new THREE.TextureLoader().load('bumpity.jpg');
 //BUMP
 const planetBump = new THREE.TextureLoader().load('planetbump.jpg');
 
 //SUN
-var sunGeometry = new THREE.TorusGeometry(1.3, 3, 10, 100);
+var sunGeometry = new THREE.TetrahedronGeometry(10.659, 3);
 var sunSkin = new THREE.TextureLoader().load("sun.png");
-var sunTexture = new THREE.TextureLoader().load("sunnormal.png");
+var sunTexture = new THREE.TextureLoader().load("sunny.jpg");
 var sunMaterial = new THREE.MeshStandardMaterial({ 
   map: sunSkin, 
-  normalMap: sunTexture,
+  normalMap: planetTextureTwo,
+  bumpMap: planetTextureTwo,
   displacementMap: sunTexture,
-  roughness: 0.9,
-  metalness: 0.1,
+  roughness: 0.7,
+  metalness: 8,
   antialias: true
 
  });
 var sun = new THREE.Mesh(sunGeometry, sunMaterial);
-sun.position.set(5, -2, -5);
+sun.position.set(145, -122, -5);
 scene.add(sun);
 
 //STAR
-var starGeometry = new THREE.IcosahedronGeometry(10, 0);
-var starTexture = new THREE.TextureLoader().load("sun.jpg");
+var starGeometry = new THREE.TetrahedronGeometry(2.2, 1);
+//var starTexture = new THREE.TextureLoader().load("sun.jpg");
 var starMaterial = new THREE.MeshStandardMaterial({ 
-  map: starTexture,
+  //map: starTexture,
   bumpMap: planetTexture, 
   metalness: 0.9, 
-  roughness: 0.1, 
-  color: 0xFFFFFF,
+  roughness: 0.5, 
+  metalness: 0.8,
   antialias: true
   
  });
-var star = new THREE.Mesh(starGeometry, starMaterial);
+
 
 // create 1000 stars and add them to the scene
-for (let i = 0; i < 100; i++) {
+var star = new THREE.Mesh(starGeometry, starMaterial);
+for (let i = 0; i < 20000; i++) {
   // create a star mesh
   const star = new THREE.Mesh(starGeometry, starMaterial);
 
   // set the position of the star to a random point within a cube centered at the origin
-  star.position.x = THREE.MathUtils.randFloatSpread(2000);
-  star.position.y = THREE.MathUtils.randFloatSpread(2000);
-  star.position.z = THREE.MathUtils.randFloatSpread(2000);
-
+  star.position.x = THREE.MathUtils.randFloatSpread(8000);
+  star.position.y = THREE.MathUtils.randFloatSpread(8000);
+  star.position.z = THREE.MathUtils.randFloatSpread(8000);
+  
   // add the star mesh to the scene
+  star.material.color.set('0x9DEE06');
   scene.add(star);
 }
 
 //MERCURY
-var mercuryGeometry = new THREE.SphereGeometry(1.022, 64, 20, 0, 6.28, 0, 3.42);
+var mercuryGeometry = new THREE.TetrahedronGeometry(1, 5);
 var mercuryTexture = new THREE.TextureLoader().load("mercury.jpg");
 var mercuryMaterial = new THREE.MeshStandardMaterial({
   map: mercuryTexture,
   normalMap: planetTexture,
   bumpMap: planetTexture,
-  antialias: true
+  antialias: true,
+  roughness: 0.9,
+  metalness: 0.9
   
 });
 var mercury = new THREE.Mesh(mercuryGeometry, mercuryMaterial);
@@ -109,8 +125,8 @@ var venusTexture = new THREE.TextureLoader().load("venus.jpg");
 var venusMaterial = new THREE.MeshStandardMaterial({
   map: venusTexture,
   normalMap: planetTexture,
-  roughness: 0,
-  metalness: 0,
+  roughness: 0.9,
+  metalness: 0.9,
   antialias: true
 
 });
@@ -126,6 +142,8 @@ var planetMaterial = new THREE.MeshStandardMaterial({
   map: earthSkin,
   normalMap: earthTexture,
   bumpMap: earthTexture,
+  roughness: 0.0,
+  metalness: 0.3,
   antialias: true
 });
 var earth = new THREE.Mesh(planet, planetMaterial);
@@ -140,6 +158,8 @@ const moonMaterial = new THREE.MeshStandardMaterial({
   map: moonSkin,
   normalMap: moonTexture,
   bumpMap: moonTexture,
+  roughness: 0.9,
+  metalness: 0.3,
   antialias: true
 });
 const moon = new THREE.Mesh(moonShape, moonMaterial);
@@ -199,6 +219,8 @@ var uranusGeometry  = new THREE.SphereGeometry(2.022, 64, 20, 0, 6.28, 0, 3.42);
 var uranusTexture = new THREE.TextureLoader().load("uranus.jpg");
 var uranusMaterial = new THREE.MeshStandardMaterial({
   map: uranusTexture,
+  normalMap: moonTexture,
+  bumpMap: moonTexture,
   antialias: true
 });
 var uranus = new THREE.Mesh(uranusGeometry, uranusMaterial);
@@ -210,6 +232,8 @@ var neptuneGeometry  = new THREE.SphereGeometry(2.022, 64, 20, 0, 6.28, 0, 3.42)
 var neptuneTexture = new THREE.TextureLoader().load("neptune.jpg");
 var neptuneMaterial = new THREE.MeshStandardMaterial({
   map: neptuneTexture,
+  normalMap: planetTextureTwo,
+  bumpMap: planetTextureTwo,
   antialias: true
 });
 var neptune = new THREE.Mesh(neptuneGeometry, neptuneMaterial);
@@ -238,14 +262,22 @@ var speed = 0.1;
 
 function animate() {
   //SUN
-  sun.rotation.x = -0.005;
-  sun.rotation.y += 0.003;
+  sun.rotation.x += 0.003;
+  sun.rotation.y += 0.001;
   sun.rotation.z += 0.002;
+
+  //STARS
+  star.rotation.x += 0.003;
+  star.rotation.y += 0.001;
+  star.rotation.z += 0.002;
 
   //MERCURY
   mercury.rotation.x = -0.005;
   mercury.rotation.y += 0.003;
   mercury.rotation.z += 0.002;
+
+  //VENUS
+  venus.rotation.y += 0.01;
 
   //EARTH
   earth.rotation.y += 0.005;
@@ -270,7 +302,7 @@ function animate() {
 
   //NEPTUNE
   neptune.rotation.x = -0.005;
-  neptune.rotation.y += 0.050;
+  neptune.rotation.y += 0.005;
 
   //SPACESHIP
   position.x += speed;
@@ -296,7 +328,6 @@ function animate() {
   spaceship.rotation.y += 0.01;
   spaceship.rotation.z += 0.02;
 
-  venus.rotation.y += 0.01;
 
   controls.update();
 
@@ -304,4 +335,36 @@ function animate() {
   requestAnimationFrame(animate);
 }
 
+//RAYCASTING
+const raycaster = new THREE.Raycaster();
+const pointer = new THREE.Vector2();
+function onPointerMove( event ) {
+  pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+  pointer.y = ( event.clientY / window.innerHeight ) * 2 + 1;
+}
+function render() {
+  raycaster.setFromCamera(pointer, camera);
+  const intersects = raycaster.intersectObjects( scene.children );
+  for ( let i = 0; i < intersects.length; i ++ ) {
+
+		intersects[ i ].object.material.color.set( 0xff0000 );
+
+	}
+
+	renderer.render( scene, camera );
+
+}
+
+window.addEventListener( 'pointermove', onPointerMove );
+
+window.requestAnimationFrame(render);
+
+
 animate();
+
+const headerDisplay = document.querySelectorAll('h1');
+
+earth.on('mouseover', function(display) {
+  headerDisplay.InnerText("Earth");
+
+})
